a7cf4b2e8886627dc678f04d795594100e7bbcde
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-07-06 10:54:43 +0100

    Apply functionalities to checkbox

--- src/index.js
+++ src/index.js
@@ -17,24 +17,34 @@ class myObject {
 const toDoArray = [];
 
 const addToDo = (todoValue) => {
-  const todo = document.createElement('li');
+  const todo = document.createElement('div');
   todo.classList.add('todo');
-  todo.innerHTML = `
-     <div class="todo-content">
-  <input type="checkbox">
+  todo.innerHTML += `
+  <input type="checkbox" class='checkbox'>
   <h2>${todoValue}</h2>
-</div>
-<div class="icon">
 <i class="fa-solid fa-ellipsis-vertical"></i>
-</div>
+<i class="fa-solid fa-trash-can"></i>
+
   `;
   todoLists.appendChild(todo);
+  
+ const checkbox = document.querySelectorAll('.checkbox');
+ checkbox.forEach(i =>{
+   i.addEventListener('click', () => {
+    i.parentElement.classList.toggle('toggle-container')
+    i.nextElementSibling.classList.toggle('checkTodo')
+    i.parentElement.lastElementChild.classList.toggle('trash-active')
+    i.parentElement.lastElementChild.previousElementSibling.classList.toggle('edit-disable')
+   })
+ })
+
 };
 
 textInput.addEventListener('keypress', e => {
   if (e.key === 'Enter' && textInput.value) {
     e.preventDefault()
     addToDo(textInput.value);
+    textInput.value = ''
   }
 
 })
